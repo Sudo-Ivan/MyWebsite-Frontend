23b29258a428282557f1cf830282056e4ba631a3
Ivan <ivan@ivanryan.dev>
2022-11-27 18:21:50 +0900

    style: text color fixes

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -29,6 +29,11 @@ export default {
         content:
           'I am a freelance software engineer and current founder and CEO of Voltaic hosting. My main interests are web development, operating systems and productivity tools. This is my personal website which showcases a collection of projects that I have worked on over the years.',
       },
+      {
+        property: 'og:image',
+        content:
+          'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d5db3d8d-896c-4432-adf9-a9c05a2ff362/52d8462e-a143-486c-8202-7b857426b144?org_if_sml=1',
+      },
     ],
   },
 }
