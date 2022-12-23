69326124575059b4d94f422e6497058132ce89a7
Ivan <ivan@ivanryan.dev>
2022-12-24 00:34:20 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import LottieVuePlayer from '@lottiefiles/vue-lottie-player'
 
-Vue.use(LottieVuePlayer)
 Vue.config.productionTip = false
 
 new Vue({
